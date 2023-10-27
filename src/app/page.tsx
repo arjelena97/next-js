c661fdb2cc8110dd5b06f5e5ffc96ddd81480410
arjelena97 <arjelena97@hotmail.com>
2023-10-27 15:36:19 +0200

    button and link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import styles from './page.module.css'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Button} from '@mui/material';
+import Link from 'next/link';
 
 export default function Home() {
+
   return (
-    <main>
+    <>
     <Box textAlign="center">
-      <Typography variant="h1">
+      <Typography variant="h3">
         WELCOME
       </Typography>
     </Box>
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
     <Box className={styles.card} >
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industries standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </Box> 
@@ -18,6 +21,11 @@ export default function Home() {
       <p><strong>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there is not anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</strong></p>
     </Box> 
     </Box>
-    </main>
+    <Link href="/FirstPage">
+    <Box sx={{ textAlign: 'center' }}>
+    <Button variant="contained" color="primary" className={styles.button}>LLAAL</Button>
+    </Box>
+    </Link>
+    </>
   )
 }
